c9f6be2229697cd67fd9d3eb913180b71cb9e35c
Arnaud Aliès <arnaudalies.py@gmail.com>
2012-06-24 09:52:51 -1000

    Update master

--- lib/vdm.py
+++ lib/vdm.py
@@ -1,15 +1,18 @@
 import urllib
 
-def random_story():
-	'''Get a story from vdm'''
-	chars_to_delete = ['</a><a href="', 'class="fmllink">', "/sante/'", "/sexe/", "/travail/", "/animaux/",
-	"</a>", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "/inclassable/", "/amour/",
-	 "/enfants/", "/argent/", '"', "?quot;"]
-	page = urllib.urlopen("http://www.viedemerde.fr/aleatoire").read()
-	story = (page[page.find('class="fmllink">')+16:page.find('" class="fmllink"> VDM</a>')+26])
-	del page
-	for x in chars_to_delete:
-		story = story.replace(x, "")
-	if (350 >= len(story)):
-		return story
-	return random_story()
+class vdm:
+	def __init__(self):
+		try:
+			self.page = urllib.urlopen("http://feeds.feedburner.com/viedemerde").read()
+		except IOError:
+			self.page = ''
+	def new_story(self):
+		start_quote = self.page.find("Aujourd'hui, ")
+		end_quote = self.page.find(". VDM")+1
+		vdm = self.page[start_quote:end_quote]
+		self.page = self.page[end_quote:]
+		if len(vdm) >= 170:
+			return self.new_story()
+		elif vdm == '':
+			return None
+		return vdm
