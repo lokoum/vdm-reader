af4c290c78438059d87dc9077c77d78b987b70b9
Arnaud Aliès <arnaudalies.py@gmail.com>
2012-06-24 10:10:53 -1000

    Update master

--- lib/vdm.py
+++ lib/vdm.py
@@ -7,12 +7,26 @@ class vdm:
 		except IOError:
 			self.page = ''
 	def new_story(self):
+		'''The fastest and the recomended option'''
 		start_quote = self.page.find("Aujourd'hui, ")
-		end_quote = self.page.find(". VDM")+1
+		end_quote = self.page.find(". VDM")+5
 		vdm = self.page[start_quote:end_quote]
 		self.page = self.page[end_quote:]
-		if len(vdm) >= 170:
+		if len(vdm) >= 310:
 			return self.new_story()
-		elif vdm == '':
-			return None
 		return vdm
+
+	def random_story(self):
+		'''This option is beta but useful if the feedburner page is already read'''
+		'''Get a story from vdm'''
+		chars_to_delete = ['</a><a href="', 'class="fmllink">', "/sante/'", "/sexe/", "/travail/", "/animaux/",
+		"</a>", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "/inclassable/", "/amour/",
+		 "/enfants/", "/argent/", '"', "?quot;"]
+		page = urllib.urlopen("http://www.viedemerde.fr/aleatoire").read()
+		story = (page[page.find('class="fmllink">')+16:page.find('" class="fmllink"> VDM</a>')+26])
+		del page
+		for x in chars_to_delete:
+			story = story.replace(x, "")
+		if (310 >= len(story)):
+			return story
+		return self.random_story()
