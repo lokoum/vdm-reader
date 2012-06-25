44eda655f8c7da32f8faa53ed29cf61d6c855917
Arnaud Aliès <arnaudalies.py@gmail.com>
2012-06-24 20:44:32 -1000

    Update master

--- uivdm.py
+++ uivdm.py
@@ -1,52 +1,68 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import wx#YOU MUST DOWNLOAD WXPYTHON @ http://wxpython.org/download.php
-import urllib
-from lib.vdm import * 
+
+import wx
+from lib.vdm import *
 
 
 class MyFrame(wx.Frame):
 	def __init__(self, *args, **kwds):
-		self.last_size = 280
+		self.choix = 1
+		self.last_size = 200
 		self.vdm = vdm()
 		kwds["style"] = wx.DEFAULT_FRAME_STYLE
 		wx.Frame.__init__(self, *args, **kwds)
 		self.output = wx.StaticText(self, -1, "")
-		self.NouvelleHistoire = wx.Button(self, -1, "Nouvelle Histoire")
+		self.xchoix = wx.ListBox(self, -1, choices=["Histoire du jour", "Histoire Aleatoire"])
+		self.enter = wx.Button(self, -1, "Nouvelle VDM")
 
 		self.__set_properties()
 		self.__do_layout()
 
-		self.Bind(wx.EVT_BUTTON, self.nouvelle_histoire, self.NouvelleHistoire)
+		self.Bind(wx.EVT_LISTBOX, self.achoix, self.xchoix)
+		self.Bind(wx.EVT_BUTTON, self.nouvelleVDM, self.enter)
 
 	def __set_properties(self):
 		self.SetTitle("VDM [BETA]")
-		self.output.SetMinSize((400, 140))
-		self.output.SetFocus()
+		self.xchoix.SetSelection(0)
 
 	def __do_layout(self):
 		sizer_1 = wx.BoxSizer(wx.VERTICAL)
 		grid_sizer_1 = wx.GridSizer(2, 1, 0, 0)
-		grid_sizer_1.Add(self.output, 0, wx.ADJUST_MINSIZE, 0)
-		grid_sizer_1.Add(self.NouvelleHistoire, 0, wx.ALIGN_CENTER_HORIZONTAL|wx.ALIGN_CENTER_VERTICAL|wx.ADJUST_MINSIZE, 0)
+		grid_sizer_2 = wx.GridSizer(1, 2, 0, 0)
+		grid_sizer_1.Add(self.output, 0, wx.EXPAND|wx.ADJUST_MINSIZE, 0)
+		grid_sizer_2.Add(self.xchoix, 0, wx.EXPAND|wx.ADJUST_MINSIZE, 0)
+		grid_sizer_2.Add(self.enter, 0, wx.ALIGN_CENTER_HORIZONTAL|wx.ALIGN_CENTER_VERTICAL|wx.ADJUST_MINSIZE, 0)
+		grid_sizer_1.Add(grid_sizer_2, 1, wx.EXPAND, 0)
 		sizer_1.Add(grid_sizer_1, 1, wx.EXPAND, 0)
 		self.SetSizer(sizer_1)
 		sizer_1.Fit(self)
 		self.Layout()
 
-	def nouvelle_histoire(self, event):
-		try:
-			story = self.vdm.new_story()
-			if story == '':
-				self.output.SetLabel(self.vdm.random_story())
-				self.last_size -= 2
-			else:
-				self.output.SetLabel(story)
-			self.last_size += 1
-			self.SetSize((400, self.last_size))
-		except IOError:
-			self.output.SetLabel("Erreur de connection")
+	def achoix(self, event):
+		if self.choix:
+			self.choix -= 1
+		else:
+			self.choix += 1
 		event.Skip()
+
+	def nouvelleVDM(self, event):
+		try:
+			if self.choix:
+				x = self.vdm.new_story()
+				if x != '':
+					self.output.SetLabel(x)
+				else:
+					self.vdm = vdm()
+			else:
+				self.output.SetLabel(self.vdm.random_story())
+		except:
+			self.output.SetLabel("Erreur")
+		self.last_size += 1
+		self.SetSize((400, self.last_size))
+		event.Skip()
+
+
 
 if __name__ == "__main__":
 	app = wx.PySimpleApp(0)
